64a240b9497bf0eb4feb01dfaa81e59560e3527c
mint <n.sogutlu@student.fontys.nl>
2025-03-19 18:24:06 +0100

    make the interval pre-determined

--- preliminary_tests/main.c
+++ preliminary_tests/main.c
@@ -1,81 +1,89 @@
+#include <pthread.h>
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <time.h>
 #include <unistd.h>
-#include <errno.h>
 
 #define NUM_OF_CORES 8
-#define MAX_PRIME 50000
+// #define MAX_PRIME 50000
+#define MAX_PRIME 100000000
 
 #define UP true
 #define DOWN false
 
-void do_primes()
-{
-    unsigned long i, num, primes = 0;
-    for (num = 1; num <= MAX_PRIME; ++num)
-   {
-        for (i = 2; (i <= num) && (num % i != 0); ++i);
-        if (i == num)
-	{
-            ++primes;
-	}
+void do_primes(int interval_in_ms) {
+  unsigned long i, num, primes = 0;
+  clock_t start;
+  clock_t end;
+
+  start = clock();
+  for (num = 1; num <= MAX_PRIME; ++num) {
+    for (i = 2; (i <= num) && (num % i != 0); ++i)
+      ;
+    if (i == num) {
+      ++primes;
     }
-    printf("Calculated %d primes.\n", primes);
+    end = clock();
+    if ((end - start) >= interval_in_ms) {
+      printf("Took: %lums\n", end - start);
+      return;
+    }
+  }
+  // printf("Calculated %lu primes.\n", primes);
 }
 
-int send(void)
-{
-    time_t start, end;
-    time_t run_time;
-    unsigned long i;
-    pid_t pids[NUM_OF_CORES];
+clock_t send(pid_t pids[NUM_OF_CORES], int interval) {
+  clock_t start, end;
+  clock_t run_time;
+  unsigned long i;
 
-    /* start of test */
-    start = time(NULL);
-    for (i = 0; i < NUM_OF_CORES; ++i) {
-        if (!(pids[i] = fork())) {
-            do_primes();
-            exit(0);
-        }
-        if (pids[i] < 0) {
-            perror("Fork");
-            exit(1);
-        }
+  start = clock();
+  for (i = 0; i < NUM_OF_CORES; ++i) {
+    if (!(pids[i] = fork())) {
+      do_primes(interval);
+      exit(0);
     }
-
-    for (i = 0; i < NUM_OF_CORES; ++i) {
-        waitpid(pids[i], NULL, 0);
+    if (pids[i] < 0) {
+      perror("Fork");
+      exit(1);
     }
+  }
 
-    end = time(NULL);
-    run_time = (end - start);
-    printf("This machine calculated all prime numbers under %d %d times "
-           "in %d seconds\n", MAX_PRIME, NUM_OF_CORES, run_time);
+  for (i = 0; i < NUM_OF_CORES; ++i) {
+    waitpid(pids[i], NULL, 0);
+  }
+  end = clock();
 
-    return run_time;
+  return end - start;
 
-    //return 0;
+  // return 0;
 }
 
-int main(void)
-{
-	// A = 01000001
+void s(int interval) { usleep(interval * 2); }
 
-	//calibration bits
-	int run_time = send();
-	sleep(run_time);
-	run_time = send();
-	
-	sleep(run_time);      // 0
-	send();           // 1
-	sleep(run_time);      // 0
-	sleep(run_time);      // 0
-	sleep(run_time);      // 0
-	sleep(run_time);      // 0
-	sleep(run_time);      // 0
-	send();           // 1
+typedef void *(*start_routine)(void *);
+int main(void) {
+  clock_t run_time;
+  // A = 01000001
 
-	return 0;
+  pid_t pids[NUM_OF_CORES];
+  int interval = 1 * CLOCKS_PER_SEC;
+
+  s(interval);
+  send(pids, interval);
+
+  s(interval);
+  send(pids, interval);
+  s(interval);
+
+  send(pids, interval);
+  s(interval);
+  s(interval);
+  s(interval);
+  s(interval);
+  s(interval);
+  send(pids, interval);
+
+  return 0;
 }
-
