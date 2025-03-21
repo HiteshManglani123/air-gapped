5acae3ff19e53356d4352ba4b8848fe68c3e5719
mint <nahithack@gmail.com>
2025-03-21 12:12:42 +0100

    create test: TEST(transmitter_create, interval_correctly_set)

--- preliminary_tests/tests/test_main.cpp
+++ preliminary_tests/tests/test_main.cpp
@@ -1,10 +1,16 @@
-// tests/test_main.cpp
 #include <gtest/gtest.h>
 
-// Example test case
-TEST(Example_Test, interval_correctly_set) {
-  EXPECT_EQ(7 * 6, 42);
+extern "C" {
+  #include <transmitter.h>
 }
+
+TEST(transmitter_create, interval_correctly_set) {
+  int interval = 1 * CLOCKS_PER_SEC;
+  struct Transmitter *transmitter = transmitter_create(interval);
+
+  EXPECT_EQ(transmitter->interval, interval);
+}
+
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
