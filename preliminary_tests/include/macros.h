1728c6528f699dbb201441cebe3da51ecb416f07
mint <nahithack@gmail.com>
2025-03-24 10:49:37 +0100

    define container_of macro

--- preliminary_tests/include/macros.h
+++ preliminary_tests/include/macros.h
@@ -4,4 +4,8 @@
 // #define MAX_PRIME 50000
 #define MAX_PRIME 100000000000
 
+#define container_of(ptr, type, member)({			\
+	const typeof(((type *)0)->member) *__mptr = (ptr);	\
+	(type *)((char *)__mptr - offsetof(type, member)) ; })
+
 #endif // MACROS_H
